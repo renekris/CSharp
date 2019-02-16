0f6a6c3207f96283a192135469936e80ef20428f
Renekris <renekrispohlak@gmail.com>
2019-02-16 16:05:27 +0200

    Replaced fors with switches + console will stay open after pressing a button not listed

--- ConsoleClicker/ConsoleClicker/Program.cs
+++ ConsoleClicker/ConsoleClicker/Program.cs
@@ -28,12 +28,13 @@ namespace ConsoleClicker
             //        break;
             //}
             int powerx = 1, count1 = 0;
-            float price1f = 50f, bars = 0f;
+            float price1f = 50f, bars = 0f, menuSelection = 0;
             Console.TreatControlCAsInput = true;
             Console.Title = "Console Clicker, Made by Renekris";
             Console.WriteLine("Welcome to Console Clicker - WIP\n\n");
             Console.ReadKey();
-            
+
+
         //Menu
         menu:
             //while (true)
@@ -57,67 +58,88 @@ namespace ConsoleClicker
 
             Console.Clear();
             Console.WriteLine("~~~Welcome~~~");
-            Console.WriteLine("<-/-> Arrows to Collect Bars \nESC - Store");
-            cki = Console.ReadKey();
-            while (cki.Key == ConsoleKey.RightArrow || cki.Key == ConsoleKey.LeftArrow)
+            Console.WriteLine("<- / -> Arrows to Collect Bars \nESC / M - Market");
+            ConsoleKey press = Console.ReadKey(true).Key;
+            switch (press)
             {
-                bars:
-                Console.Clear();
-                Console.WriteLine("Press <- and -> arrows to earn Bars ~{0:n2}\nPress ESC to go back to the menu.", bars);
-                cki = Console.ReadKey();
-                if (cki.Key == ConsoleKey.Escape)
-                {
+                case ConsoleKey.LeftArrow:
+                case ConsoleKey.RightArrow:
+                    menuSelection = 1;
+                    break;
+                case ConsoleKey.Escape:
+                case ConsoleKey.M:
+                    menuSelection = 2;
+                    break;
+                default:
                     goto menu;
-                }
-                System.Threading.Thread.Sleep(9);
-                if (cki.Key == ConsoleKey.LeftArrow && count1 == 0)
-                {
-                    bars += 1 * powerx;
-                    count1 = 1;
-                    goto bars;
-                }
-
-                if (cki.Key == ConsoleKey.RightArrow && count1 == 1)
-                {
-                    bars += 1 * powerx;
-                    count1 = 0;
-                    goto bars;
-                }
-                else {
-                    goto bars;
-                }
             }
-            //Store
-            while (cki.Key == ConsoleKey.Escape)
+            //Bars
+            while (menuSelection == 1)
+            {
+            bars:
+                Console.Clear();
+                Console.WriteLine("Press <- and -> arrows to earn Bars ~{0:n2}\nPress ESC to go back to the menu.\n", bars);
+                press = Console.ReadKey(true).Key;
+                //Bar Values
+                switch (press)
+                {
+                    case ConsoleKey.Escape:
+                        goto menu;
+                    case ConsoleKey.LeftArrow:
+                        if (press == ConsoleKey.LeftArrow && count1 == 0)
+                        {
+                            bars += 1 * powerx;
+                            count1 = 1;
+                            goto bars;
+                        }
+                        else
+                        break;
+                    case ConsoleKey.RightArrow:
+                        if (press == ConsoleKey.RightArrow && count1 == 1)
+                        {
+                            bars += 1 * powerx;
+                            count1 = 0;
+                            goto bars;
+                        }
+                        else
+                        break;
+                    default:
+                        goto bars;
+                }
+                System.Threading.Thread.Sleep(10);
+            }
+            //Market
+            while (menuSelection == 2)
             {
                 store:
                 Console.Clear();
                 Console.WriteLine("~~Welcome to the store~~\nYou have about ~{0:n2} bars.\nESC to menu\nEnter a number to buy\n", bars);
                 Console.WriteLine("1.\t+= 1x enter power\n\t~{0:n2} Bars\n\n", price1f);
-                cki = Console.ReadKey();
-                if (cki.Key == ConsoleKey.D1)
+                press = Console.ReadKey(true).Key;
+                switch (press)
                 {
-                    if (bars >= price1f)
-                    {
-                        bars = bars - price1f;
-                        price1f = price1f * 1.21f;
-                        powerx += 1;
-                        Console.Clear();
-                        Console.WriteLine("You have just bought += 1x power\n~{0:n2} Bars remaining.\n\nESC to go back.", bars);
-                        Console.ReadKey();
+                    case ConsoleKey.D1:
+                        if (bars >= price1f)
+                        {
+                            bars = bars - price1f;
+                            price1f = price1f * 1.21f;
+                            powerx += 1;
+                            Console.Clear();
+                            Console.WriteLine("You have just bought += 1x power\n~{0:n2} Bars remaining.\n\nESC to go back.", bars);
+                            Console.ReadKey();
+                            goto store;
+                        }
+                        else
+                        {
+                            Console.Clear();
+                            Console.WriteLine("You don't have enough Bars!");
+                            Console.ReadKey();
+                            goto store;
+                        }
+                    case ConsoleKey.Escape:
+                        goto menu;
+                    default:
                         goto store;
-                    }
-                    else
-                    {
-                        Console.Clear();
-                        Console.WriteLine("You don't have enough Bars!");
-                        Console.ReadKey();
-                        goto store;
-                    }
-                }
-                else if (cki.Key == ConsoleKey.Escape)
-                {
-                    goto menu;
                 }
             }
         }
