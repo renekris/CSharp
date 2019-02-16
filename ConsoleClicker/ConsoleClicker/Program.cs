a7aae526daec54d0746e411d812d6a800372d27c
Renekris <renekrispohlak@gmail.com>
2019-02-16 02:32:58 +0200

    Bar earning and a simple store

--- ConsoleClicker/ConsoleClicker/Program.cs
+++ ConsoleClicker/ConsoleClicker/Program.cs
@@ -10,7 +10,66 @@ namespace ConsoleClicker
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Console Clicker - WIP");
+            Console.Title = "Console Clicker, Made by Renekris";
+            int bars = 0, x = 1;
+            Console.WriteLine("Welcome to Console Clicker - WIP\n\n");
+            Console.ReadKey();
+            ConsoleKeyInfo cki;
+        //Menu
+        menu:
+            Console.Clear();
+            Console.WriteLine("~~Main screen!~~");
+            Console.WriteLine("Enter - collect bars \nEsc - store");
+            cki = Console.ReadKey();
+            while (cki.Key == ConsoleKey.Enter)
+            {
+                Console.Clear();
+                if (cki.Key == ConsoleKey.Enter)
+                {
+                    bars += 1 * x;
+                    Console.WriteLine("Press enter to earn bars ~{0}\nPress esc to go back to the menu.", bars);
+                }
+                
+                cki = Console.ReadKey();
+                if (cki.Key == ConsoleKey.Escape)
+                {
+                    goto menu;
+                }
+                System.Threading.Thread.Sleep(9);
+            }
+            //Store
+            while (cki.Key == ConsoleKey.Escape)
+            {
+                store:
+                Console.Clear();
+                Console.WriteLine("~~Welcome to the store~~\nYou have about ~{0} bars.\nEsc to menu\nEnter a number to buy\n", bars);
+                Console.WriteLine("1. 2x enter power");
+                cki = Console.ReadKey();
+                if (cki.Key == ConsoleKey.D1)
+                {
+                    if (bars >= 50)
+                    {
+                        x = 2;
+                        bars = bars - 50;
+                        Console.Clear();
+                        Console.WriteLine("You have just bought 2x power\n~{0} bars remaining.\n\nEsc to go back.", bars);
+                        Console.ReadKey();
+                        goto store;
+                    }
+                    else
+                    {
+                        Console.Clear();
+                        Console.WriteLine("You dont have enough bars!");
+                        Console.ReadKey();
+                        goto store;
+                    }
+                }
+                else if (cki.Key == ConsoleKey.Escape)
+                {
+                    goto menu;
+                }
+            }
         }
     }
 }
+
