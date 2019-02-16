f2812fd8a77d83c9ec95fbe639cb0f3f093f93e6
Renekris <renekrispohlak@gmail.com>
2019-02-16 21:28:44 +0200

    Bars limit + counters

--- ConsoleClicker/ConsoleClicker/Program.cs
+++ ConsoleClicker/ConsoleClicker/Program.cs
@@ -23,8 +23,8 @@ namespace ConsoleClicker
 
         static void Main(string[] args)
         {
-            int powerx = 1, count1 = 0;
-            float price1f = 50f, price2f = 100f, bars = 0f, totalBars = 0f, menuSelection = 0, timedx = 0f;
+            int powerx = 1, count1 = 0, countPowerX = 0, countBarsLimit = 0;
+            float barsLimit = 50f, price1f = 50f, price2f = 100f, price3f = 10, bars = 0f, totalBars = 0f, menuSelection = 0, timedx = 0f;
             bool isSecret = false, side = false;
             Console.TreatControlCAsInput = true;
             Console.Title = "Console Clicker, Made by Renekris";
@@ -70,7 +70,7 @@ namespace ConsoleClicker
                 Console.Clear();
                 Console.WriteLine("Press the arrow shown below, to earn Bars.\nPress ESC to go back to the menu.\n");
             bars:
-                Console.WriteLine("~{0:n2} Bars", bars);
+                Console.WriteLine("~{0:n2}/{1} Bars", bars, barsLimit);
                 if (side == false)
                 {
                     Console.WriteLine("  <-");
@@ -91,7 +91,7 @@ namespace ConsoleClicker
                     case ConsoleKey.Escape:
                         goto menu;
                     case ConsoleKey.LeftArrow:
-                        if (press == ConsoleKey.LeftArrow && count1 == 0)
+                        if (press == ConsoleKey.LeftArrow && count1 == 0 && barsLimit > bars)
                         {
                             side = true;
                             bars += 1 * powerx;
@@ -103,7 +103,7 @@ namespace ConsoleClicker
                         else
                             goto bars;
                     case ConsoleKey.RightArrow:
-                        if (press == ConsoleKey.RightArrow && count1 == 1)
+                        if (press == ConsoleKey.RightArrow && count1 == 1 && barsLimit > bars)
                         {
                             side = false;
                             bars += 1 * powerx;
@@ -124,15 +124,17 @@ namespace ConsoleClicker
                 store:
                 Console.Clear();
                 Console.WriteLine("~~Welcome to the store~~\nYou have about ~{0:n2} bars.\nESC to menu\nEnter a number to buy\n", bars);
-                Console.WriteLine("1.\t+= 1x enter power\n\t~{0:n2} Bars\n\n", price1f);
+                Console.WriteLine("1.\t+= 1x enter power\n\t~{0:n2} Bars\n\tYou have bought {1}/10 of this\n\n", price1f, countPowerX);
                 Console.WriteLine("2.\tSecret\n\t~{0:n2} Bars\n\n", price2f);
+                Console.WriteLine("3.\tBars limit currently: {0}\n\t~{1:n2} Bars\n\tYou have bought {2}/50 of this", barsLimit, price3f, countBarsLimit);
                 press = Console.ReadKey(true).Key;
                 switch (press)
                 {
                     //Power upgrade
                     case ConsoleKey.D1:
-                        if (bars >= price1f)
+                        if (bars >= price1f && countPowerX < 10)
                         {
+                            countPowerX++;
                             bars = bars - price1f;
                             price1f = price1f * 1.21f;
                             powerx += 1;
@@ -165,7 +167,26 @@ namespace ConsoleClicker
                             Console.WriteLine("You don't have enough Bars!");
                             Console.ReadKey();
                             goto store;
-                        } 
+                        }
+                    case ConsoleKey.D3:
+                        if (bars >= price3f && countBarsLimit < 50)
+                        {
+                            countBarsLimit++;
+                            bars = bars - price3f;
+                            price3f = price3f * 1.25f;
+                            barsLimit = barsLimit * 1.25f;
+                            Console.Clear();
+                            Console.WriteLine("You have just increased your Bars limit\n~{0:n2} Bars remaining.\n\nESC to go back.", bars);
+                            Console.ReadKey();
+                            goto store;
+                        }
+                        else
+                        {
+                            Console.Clear();
+                            Console.WriteLine("You don't have enough Bars!");
+                            Console.ReadKey();
+                            goto store;
+                        }
                     case ConsoleKey.Escape:
                         goto menu;
                     default:
