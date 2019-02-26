854887eff4ad6ae3699a391c441c6c696c1d891d
Renekris <renekrispohlak@gmail.com>
2019-02-26 12:08:00 +0200

    All cards implemented

--- Personal/Cards/Cards/Program.cs
+++ Personal/Cards/Cards/Program.cs
@@ -2,11 +2,12 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Cards
 {
-    public static class Cards
+    public class Cards
     {
         private static string RedCard(string card)
         {
@@ -18,89 +19,200 @@ namespace Cards
 
         private static string WhiteCard(string card)
         {
-            Console.ForegroundColor = ConsoleColor.DarkGray;
+            Console.ForegroundColor = ConsoleColor.Gray;
             Console.WriteLine(card);
             Console.ResetColor();
             return card;
         }
 
-        public static void SpadeCards()
+        public static string SpadeCards(int num)
         {
-             string spades1Card = WhiteCard(" _____\r\n|1    |\r\n|     |\r\n|  ♠  |\r\n|     |\r\n|     |\r\n ‾‾‾‾‾");
-             string spades2Card = WhiteCard(" _____\r\n|2    |\r\n|  ♠  |\r\n|     |\r\n|  ♠  |\r\n|     |\r\n ‾‾‾‾‾");
-             string spades3Card = WhiteCard(" _____\r\n|3    |\r\n|  ♠  |\r\n|  ♠  |\r\n|  ♠  |\r\n|     |\r\n ‾‾‾‾‾");
-             string spades4Card = WhiteCard(" _____\r\n|4    |\r\n| ♠ ♠ |\r\n|     |\r\n| ♠ ♠ |\r\n|     |\r\n ‾‾‾‾‾");
-             string spades5Card = WhiteCard(" _____\r\n|5    |\r\n| ♠ ♠ |\r\n|  ♠  |\r\n| ♠ ♠ |\r\n|     |\r\n ‾‾‾‾‾");
-             string spades6Card = WhiteCard(" _____\r\n|6    |\r\n| ♠ ♠ |\r\n| ♠ ♠ |\r\n| ♠ ♠ |\r\n|     |\r\n ‾‾‾‾‾");
-             string spades7Card = WhiteCard(" _____\r\n|7    |\r\n| ♠ ♠ |\r\n|♠ ♠ ♠|\r\n| ♠ ♠ |\r\n|     |\r\n ‾‾‾‾‾");
-             string spades8Card = WhiteCard(" _____\r\n|8    |\r\n|♠ ♠ ♠|\r\n| ♠ ♠ |\r\n|♠ ♠ ♠|\r\n|     |\r\n ‾‾‾‾‾");
-             string spades9Card = WhiteCard(" _____\r\n|9 ♠  |\r\n|♠   ♠|\r\n|♠ ♠ ♠|\r\n|♠   ♠|\r\n|  ♠  |\r\n ‾‾‾‾‾");
-            string spades10Card = WhiteCard(" _____\r\n|10♠  |\r\n|♠ ♠ ♠|\r\n| ♠ ♠ |\r\n|♠ ♠ ♠|\r\n|  ♠  |\r\n ‾‾‾‾‾");
-            string spades11Card = WhiteCard(" _____\r\n|J    |\r\n| ♠-♠ |\r\n| |J| |\r\n| ♠-♠ |\r\n|     |\r\n ‾‾‾‾‾");
-            string spades12Card = WhiteCard(" _____\r\n|K    |\r\n| ♠-♠ |\r\n| |K| |\r\n| ♠-♠ |\r\n|     |\r\n ‾‾‾‾‾");
-            string spades13Card = WhiteCard(" _____\r\n|Q    |\r\n| ♠-♠ |\r\n| |Q| |\r\n| ♠-♠ |\r\n|     |\r\n ‾‾‾‾‾");
-        }
-        public static void HeartCards()
-        {
-            string heart1Card = RedCard(" _____\r\n|1    |\r\n|     |\r\n|  ♥  |\r\n|     |\r\n|     |\r\n ‾‾‾‾‾");
-            string heart2Card = RedCard(" _____\r\n|2    |\r\n|  ♥  |\r\n|     |\r\n|  ♥  |\r\n|     |\r\n ‾‾‾‾‾");
-            string heart3Card = RedCard(" _____\r\n|3    |\r\n|  ♥  |\r\n|  ♥  |\r\n|  ♥  |\r\n|     |\r\n ‾‾‾‾‾");
-            string heart4Card = RedCard(" _____\r\n|4    |\r\n| ♥ ♥ |\r\n|     |\r\n| ♥ ♥ |\r\n|     |\r\n ‾‾‾‾‾");
-            string heart5Card = RedCard(" _____\r\n|5    |\r\n| ♥ ♥ |\r\n|  ♥  |\r\n| ♥ ♥ |\r\n|     |\r\n ‾‾‾‾‾");
-            string heart6Card = RedCard(" _____\r\n|6    |\r\n| ♥ ♥ |\r\n| ♥ ♥ |\r\n| ♥ ♥ |\r\n|     |\r\n ‾‾‾‾‾");
-            string heart7Card = RedCard(" _____\r\n|7    |\r\n| ♥ ♥ |\r\n|♥ ♥ ♥|\r\n| ♥ ♥ |\r\n|     |\r\n ‾‾‾‾‾");
-            string heart8Card = RedCard(" _____\r\n|8    |\r\n|♥ ♥ ♥|\r\n| ♥ ♥ |\r\n|♥ ♥ ♥|\r\n|     |\r\n ‾‾‾‾‾");
-            string heart9Card = RedCard(" _____\r\n|9 ♥  |\r\n|♥   ♥|\r\n|♥ ♥ ♥|\r\n|♥   ♥|\r\n|  ♥  |\r\n ‾‾‾‾‾");
-            string heart10Card = RedCard(" _____\r\n|10♥  |\r\n|♥ ♥ ♥|\r\n| ♥ ♥ |\r\n|♥ ♥ ♥|\r\n|  ♥  |\r\n ‾‾‾‾‾");
-            string heart11Card = RedCard(" _____\r\n|J    |\r\n| ♥-♥ |\r\n| |J| |\r\n| ♥-♥ |\r\n|     |\r\n ‾‾‾‾‾");
-            string heart12Card = RedCard(" _____\r\n|K    |\r\n| ♥-♥ |\r\n| |K| |\r\n| ♥-♥ |\r\n|     |\r\n ‾‾‾‾‾");
-            string heart13Card = RedCard(" _____\r\n|Q    |\r\n| ♥-♥ |\r\n| |Q| |\r\n| ♥-♥ |\r\n|     |\r\n ‾‾‾‾‾");
-        }
-        public static void DiamondCards()
-        {
-            string diamond1Card = RedCard(" _____\r\n|1    |\r\n|     |\r\n|  ♦  |\r\n|     |\r\n|     |\r\n ‾‾‾‾‾");
-            string diamond2Card = RedCard(" _____\r\n|2    |\r\n|  ♦  |\r\n|     |\r\n|  ♦  |\r\n|     |\r\n ‾‾‾‾‾");
-            string diamond3Card = RedCard(" _____\r\n|3    |\r\n|  ♦  |\r\n|  ♦  |\r\n|  ♦  |\r\n|     |\r\n ‾‾‾‾‾");
-            string diamond4Card = RedCard(" _____\r\n|4    |\r\n| ♦ ♦ |\r\n|     |\r\n| ♦ ♦ |\r\n|     |\r\n ‾‾‾‾‾");
-            string diamond5Card = RedCard(" _____\r\n|5    |\r\n| ♦ ♦ |\r\n|  ♦  |\r\n| ♦ ♦ |\r\n|     |\r\n ‾‾‾‾‾");
-            string diamond6Card = RedCard(" _____\r\n|6    |\r\n| ♦ ♦ |\r\n| ♦ ♦ |\r\n| ♦ ♦ |\r\n|     |\r\n ‾‾‾‾‾");
-            string diamond7Card = RedCard(" _____\r\n|7    |\r\n| ♦ ♦ |\r\n|♦ ♦ ♦|\r\n| ♦ ♦ |\r\n|     |\r\n ‾‾‾‾‾");
-            string diamond8Card = RedCard(" _____\r\n|8    |\r\n|♦ ♦ ♦|\r\n| ♦ ♦ |\r\n|♦ ♦ ♦|\r\n|     |\r\n ‾‾‾‾‾");
-            string diamond9Card = RedCard(" _____\r\n|9 ♦  |\r\n|♦   ♦|\r\n|♦ ♦ ♦|\r\n|♦   ♦|\r\n|  ♦  |\r\n ‾‾‾‾‾");
-            string diamond10Card = RedCard(" _____\r\n|10♦  |\r\n|♦ ♦ ♦|\r\n| ♦ ♦ |\r\n|♦ ♦ ♦|\r\n|  ♦  |\r\n ‾‾‾‾‾");
-            string diamond11Card = RedCard(" _____\r\n|J    |\r\n| ♦-♦ |\r\n| |J| |\r\n| ♦-♦ |\r\n|     |\r\n ‾‾‾‾‾");
-            string diamond12Card = RedCard(" _____\r\n|K    |\r\n| ♦-♦ |\r\n| |K| |\r\n| ♦-♦ |\r\n|     |\r\n ‾‾‾‾‾");
-            string diamond13Card = RedCard(" _____\r\n|Q    |\r\n| ♦-♦ |\r\n| |Q| |\r\n| ♦-♦ |\r\n|     |\r\n ‾‾‾‾‾");
-        }
-        public static void ClubCards()
-        {
-            string club1Card = WhiteCard(" _____\r\n|1    |\r\n|     |\r\n|  ♣  |\r\n|     |\r\n|     |\r\n ‾‾‾‾‾");
-            string club2Card = WhiteCard(" _____\r\n|2    |\r\n|  ♣  |\r\n|     |\r\n|  ♣  |\r\n|     |\r\n ‾‾‾‾‾");
-            string club3Card = WhiteCard(" _____\r\n|3    |\r\n|  ♣  |\r\n|  ♣  |\r\n|  ♣  |\r\n|     |\r\n ‾‾‾‾‾");
-            string club4Card = WhiteCard(" _____\r\n|4    |\r\n| ♣ ♣ |\r\n|     |\r\n| ♣ ♣ |\r\n|     |\r\n ‾‾‾‾‾");
-            string club5Card = WhiteCard(" _____\r\n|5    |\r\n| ♣ ♣ |\r\n|  ♣  |\r\n| ♣ ♣ |\r\n|     |\r\n ‾‾‾‾‾");
-            string club6Card = WhiteCard(" _____\r\n|6    |\r\n| ♣ ♣ |\r\n| ♣ ♣ |\r\n| ♣ ♣ |\r\n|     |\r\n ‾‾‾‾‾");
-            string club7Card = WhiteCard(" _____\r\n|7    |\r\n| ♣ ♣ |\r\n|♣ ♣ ♣|\r\n| ♣ ♣ |\r\n|     |\r\n ‾‾‾‾‾");
-            string club8Card = WhiteCard(" _____\r\n|8    |\r\n|♣ ♣ ♣|\r\n| ♣ ♣ |\r\n|♣ ♣ ♣|\r\n|     |\r\n ‾‾‾‾‾");
-            string club9Card = WhiteCard(" _____\r\n|9 ♣  |\r\n|♣   ♣|\r\n|♣ ♣ ♣|\r\n|♣   ♣|\r\n|  ♣  |\r\n ‾‾‾‾‾");
-            string club10Card = WhiteCard(" _____\r\n|10♣  |\r\n|♣ ♣ ♣|\r\n| ♣ ♣ |\r\n|♣ ♣ ♣|\r\n|  ♣  |\r\n ‾‾‾‾‾");
-            string club11Card = WhiteCard(" _____\r\n|J    |\r\n| ♣-♣ |\r\n| |J| |\r\n| ♣-♣ |\r\n|     |\r\n ‾‾‾‾‾");
-            string club12Card = WhiteCard(" _____\r\n|K    |\r\n| ♣-♣ |\r\n| |K| |\r\n| ♣-♣ |\r\n|     |\r\n ‾‾‾‾‾");
-            string club13Card = WhiteCard(" _____\r\n|Q    |\r\n| ♣-♣ |\r\n| |Q| |\r\n| ♣-♣ |\r\n|     |\r\n ‾‾‾‾‾");
-        }
+            string returnCard = null;
+            switch (num)
+            {
+                //Space cards
+                case 1:
+                    returnCard = WhiteCard(" _____\r\n|1    |\r\n|     |\r\n|  ♠  |\r\n|     |\r\n|     |\r\n ‾‾‾‾‾");
+                    break;
+                case 2:
+                    returnCard = WhiteCard(" _____\r\n|2    |\r\n|  ♠  |\r\n|     |\r\n|  ♠  |\r\n|     |\r\n ‾‾‾‾‾");
+                    break;
+                case 3:
+                    returnCard = WhiteCard(" _____\r\n|3    |\r\n|  ♠  |\r\n|  ♠  |\r\n|  ♠  |\r\n|     |\r\n ‾‾‾‾‾");
+                    break;
+                case 4:
+                    returnCard = WhiteCard(" _____\r\n|4    |\r\n| ♠ ♠ |\r\n|     |\r\n| ♠ ♠ |\r\n|     |\r\n ‾‾‾‾‾");
+                    break;
+                case 5:
+                    returnCard = WhiteCard(" _____\r\n|5    |\r\n| ♠ ♠ |\r\n|  ♠  |\r\n| ♠ ♠ |\r\n|     |\r\n ‾‾‾‾‾");
+                    break;
+                case 6:
+                    returnCard = WhiteCard(" _____\r\n|6    |\r\n| ♠ ♠ |\r\n| ♠ ♠ |\r\n| ♠ ♠ |\r\n|     |\r\n ‾‾‾‾‾");
+                    break;
+                case 7:
+                    returnCard = WhiteCard(" _____\r\n|7    |\r\n| ♠ ♠ |\r\n|♠ ♠ ♠|\r\n| ♠ ♠ |\r\n|     |\r\n ‾‾‾‾‾");
+                    break;
+                case 8:
+                    returnCard = WhiteCard(" _____\r\n|8    |\r\n|♠ ♠ ♠|\r\n| ♠ ♠ |\r\n|♠ ♠ ♠|\r\n|     |\r\n ‾‾‾‾‾");
+                    break;
+                case 9:
+                    returnCard = WhiteCard(" _____\r\n|9 ♠  |\r\n|♠   ♠|\r\n|♠ ♠ ♠|\r\n|♠   ♠|\r\n|  ♠  |\r\n ‾‾‾‾‾");
+                    break;
+                case 10:
+                    returnCard = WhiteCard(" _____\r\n|10♠  |\r\n|♠ ♠ ♠|\r\n| ♠ ♠ |\r\n|♠ ♠ ♠|\r\n|  ♠  |\r\n ‾‾‾‾‾");
+                    break;
+                case 11:
+                    returnCard = WhiteCard(" _____\r\n|J    |\r\n| ♠-♠ |\r\n| |J| |\r\n| ♠-♠ |\r\n|     |\r\n ‾‾‾‾‾");
+                    break;
+                case 12:
+                    returnCard = WhiteCard(" _____\r\n|K    |\r\n| ♠-♠ |\r\n| |K| |\r\n| ♠-♠ |\r\n|     |\r\n ‾‾‾‾‾");
+                    break;
+                case 13:
+                    returnCard = WhiteCard(" _____\r\n|Q    |\r\n| ♠-♠ |\r\n| |Q| |\r\n| ♠-♠ |\r\n|     |\r\n ‾‾‾‾‾");
+                    break;
+                //Heart cards     
+                case 14:
+                    returnCard = RedCard(" _____\r\n|1    |\r\n|     |\r\n|  ♥  |\r\n|     |\r\n|     |\r\n ‾‾‾‾‾");
+                    break;
+                case 15:
+                    returnCard = RedCard(" _____\r\n|2    |\r\n|  ♥  |\r\n|     |\r\n|  ♥  |\r\n|     |\r\n ‾‾‾‾‾");
+                    break;
+                case 16:
+                    returnCard = RedCard(" _____\r\n|3    |\r\n|  ♥  |\r\n|  ♥  |\r\n|  ♥  |\r\n|     |\r\n ‾‾‾‾‾");
+                    break;
+                case 17:
+                    returnCard = RedCard(" _____\r\n|4    |\r\n| ♥ ♥ |\r\n|     |\r\n| ♥ ♥ |\r\n|     |\r\n ‾‾‾‾‾");
+                    break;
+                case 18:
+                    returnCard = RedCard(" _____\r\n|5    |\r\n| ♥ ♥ |\r\n|  ♥  |\r\n| ♥ ♥ |\r\n|     |\r\n ‾‾‾‾‾");
+                    break;
+                case 19:
+                    returnCard = RedCard(" _____\r\n|6    |\r\n| ♥ ♥ |\r\n| ♥ ♥ |\r\n| ♥ ♥ |\r\n|     |\r\n ‾‾‾‾‾");
+                    break;
+                case 20:
+                    returnCard = RedCard(" _____\r\n|7    |\r\n| ♥ ♥ |\r\n|♥ ♥ ♥|\r\n| ♥ ♥ |\r\n|     |\r\n ‾‾‾‾‾");
+                    break;
+                case 21:
+                    returnCard = RedCard(" _____\r\n|8    |\r\n|♥ ♥ ♥|\r\n| ♥ ♥ |\r\n|♥ ♥ ♥|\r\n|     |\r\n ‾‾‾‾‾");
+                    break;
+                case 22:
+                    returnCard = RedCard(" _____\r\n|9 ♥  |\r\n|♥   ♥|\r\n|♥ ♥ ♥|\r\n|♥   ♥|\r\n|  ♥  |\r\n ‾‾‾‾‾");
+                    break;
+                case 23:
+                    returnCard = RedCard(" _____\r\n|10♥  |\r\n|♥ ♥ ♥|\r\n| ♥ ♥ |\r\n|♥ ♥ ♥|\r\n|  ♥  |\r\n ‾‾‾‾‾");
+                    break;
+                case 24:
+                    returnCard = RedCard(" _____\r\n|J    |\r\n| ♥-♥ |\r\n| |J| |\r\n| ♥-♥ |\r\n|     |\r\n ‾‾‾‾‾");
+                    break;
+                case 25:
+                    returnCard = RedCard(" _____\r\n|K    |\r\n| ♥-♥ |\r\n| |K| |\r\n| ♥-♥ |\r\n|     |\r\n ‾‾‾‾‾");
+                    break;
+                case 26:
+                    returnCard = RedCard(" _____\r\n|Q    |\r\n| ♥-♥ |\r\n| |Q| |\r\n| ♥-♥ |\r\n|     |\r\n ‾‾‾‾‾");
+                    break;
+                //Diamond cards
+                case 27:
+                    returnCard = RedCard(" _____\r\n|1    |\r\n|     |\r\n|  ♦  |\r\n|     |\r\n|     |\r\n ‾‾‾‾‾");
+                    break;
+                case 28:
+                    returnCard = RedCard(" _____\r\n|2    |\r\n|  ♦  |\r\n|     |\r\n|  ♦  |\r\n|     |\r\n ‾‾‾‾‾");
+                    break;
+                case 29:
+                    returnCard = RedCard(" _____\r\n|3    |\r\n|  ♦  |\r\n|  ♦  |\r\n|  ♦  |\r\n|     |\r\n ‾‾‾‾‾");
+                    break;
+                case 30:
+                    returnCard = RedCard(" _____\r\n|4    |\r\n| ♦ ♦ |\r\n|     |\r\n| ♦ ♦ |\r\n|     |\r\n ‾‾‾‾‾");
+                    break;
+                case 31:
+                    returnCard = RedCard(" _____\r\n|5    |\r\n| ♦ ♦ |\r\n|  ♦  |\r\n| ♦ ♦ |\r\n|     |\r\n ‾‾‾‾‾");
+                    break;
+                case 32:
+                    returnCard = RedCard(" _____\r\n|6    |\r\n| ♦ ♦ |\r\n| ♦ ♦ |\r\n| ♦ ♦ |\r\n|     |\r\n ‾‾‾‾‾");
+                    break;
+                case 33:
+                    returnCard = RedCard(" _____\r\n|7    |\r\n| ♦ ♦ |\r\n|♦ ♦ ♦|\r\n| ♦ ♦ |\r\n|     |\r\n ‾‾‾‾‾");
+                    break;
+                case 34:
+                    returnCard = RedCard(" _____\r\n|8    |\r\n|♦ ♦ ♦|\r\n| ♦ ♦ |\r\n|♦ ♦ ♦|\r\n|     |\r\n ‾‾‾‾‾");
+                    break;
+                case 35:
+                    returnCard = RedCard(" _____\r\n|9 ♦  |\r\n|♦   ♦|\r\n|♦ ♦ ♦|\r\n|♦   ♦|\r\n|  ♦  |\r\n ‾‾‾‾‾");
+                    break;
+                case 36:
+                    returnCard = RedCard(" _____\r\n|10♦  |\r\n|♦ ♦ ♦|\r\n| ♦ ♦ |\r\n|♦ ♦ ♦|\r\n|  ♦  |\r\n ‾‾‾‾‾");
+                    break;
+                case 37:
+                    returnCard = RedCard(" _____\r\n|J    |\r\n| ♦-♦ |\r\n| |J| |\r\n| ♦-♦ |\r\n|     |\r\n ‾‾‾‾‾");
+                    break;
+                case 38:
+                    returnCard = RedCard(" _____\r\n|K    |\r\n| ♦-♦ |\r\n| |K| |\r\n| ♦-♦ |\r\n|     |\r\n ‾‾‾‾‾");
+                    break;
+                case 39:
+                    returnCard = RedCard(" _____\r\n|Q    |\r\n| ♦-♦ |\r\n| |Q| |\r\n| ♦-♦ |\r\n|     |\r\n ‾‾‾‾‾");
+                    break;
+                //Club cards
+                case 40:
+                    returnCard = WhiteCard(" _____\r\n|1    |\r\n|     |\r\n|  ♣  |\r\n|     |\r\n|     |\r\n ‾‾‾‾‾");
+                    break;
+                case 41:
+                    returnCard = WhiteCard(" _____\r\n|2    |\r\n|  ♣  |\r\n|     |\r\n|  ♣  |\r\n|     |\r\n ‾‾‾‾‾");
+                    break;
+                case 42:
+                    returnCard = WhiteCard(" _____\r\n|3    |\r\n|  ♣  |\r\n|  ♣  |\r\n|  ♣  |\r\n|     |\r\n ‾‾‾‾‾");
+                    break;
+                case 43:
+                    returnCard = WhiteCard(" _____\r\n|4    |\r\n| ♣ ♣ |\r\n|     |\r\n| ♣ ♣ |\r\n|     |\r\n ‾‾‾‾‾");
+                    break;
+                case 44:
+                    returnCard = WhiteCard(" _____\r\n|5    |\r\n| ♣ ♣ |\r\n|  ♣  |\r\n| ♣ ♣ |\r\n|     |\r\n ‾‾‾‾‾");
+                    break;
+                case 45:
+                    returnCard = WhiteCard(" _____\r\n|6    |\r\n| ♣ ♣ |\r\n| ♣ ♣ |\r\n| ♣ ♣ |\r\n|     |\r\n ‾‾‾‾‾");
+                    break;
+                case 46:
+                    returnCard = WhiteCard(" _____\r\n|7    |\r\n| ♣ ♣ |\r\n|♣ ♣ ♣|\r\n| ♣ ♣ |\r\n|     |\r\n ‾‾‾‾‾");
+                    break;
+                case 47:
+                    returnCard = WhiteCard(" _____\r\n|8    |\r\n|♣ ♣ ♣|\r\n| ♣ ♣ |\r\n|♣ ♣ ♣|\r\n|     |\r\n ‾‾‾‾‾");
+                    break;
+                case 48:
+                    returnCard = WhiteCard(" _____\r\n|9 ♣  |\r\n|♣   ♣|\r\n|♣ ♣ ♣|\r\n|♣   ♣|\r\n|  ♣  |\r\n ‾‾‾‾‾");
+                    break;
+                case 49:
+                    returnCard = WhiteCard(" _____\r\n|10♣  |\r\n|♣ ♣ ♣|\r\n| ♣ ♣ |\r\n|♣ ♣ ♣|\r\n|  ♣  |\r\n ‾‾‾‾‾");
+                    break;
+                case 50:
+                    returnCard = WhiteCard(" _____\r\n|J    |\r\n| ♣-♣ |\r\n| |J| |\r\n| ♣-♣ |\r\n|     |\r\n ‾‾‾‾‾");
+                    break;
+                case 51:
+                    returnCard = WhiteCard(" _____\r\n|K    |\r\n| ♣-♣ |\r\n| |K| |\r\n| ♣-♣ |\r\n|     |\r\n ‾‾‾‾‾");
+                    break;
+                case 52:
+                    returnCard = WhiteCard(" _____\r\n|Q    |\r\n| ♣-♣ |\r\n| |Q| |\r\n| ♣-♣ |\r\n|     |\r\n ‾‾‾‾‾");
+                    break;
+            }
 
+            return returnCard;
+        }
     }
     class Program
     {
         
         static void Main(string[] args)
         {
+            Random rng = new Random();
             Console.OutputEncoding = Encoding.Unicode;
 
-            Cards.SpadeCards();
-            Cards.ClubCards();
-            Cards.DiamondCards();
-            Cards.HeartCards();
+            while (true)
+            {
+                int rng1 = rng.Next(1, 52);
+                Cards.SpadeCards(rng1);
+                Console.ReadKey();
+                Thread.Sleep(100);
+                Console.Clear();
+            }
+            
+
             Console.WriteLine("♠\t♥\t♦\t♣");
             Console.ReadKey();
         }
