87ca2d09000dc05f9a42ff7f131e68fd7552958a
Renekris <renekrispohlak@gmail.com>
2019-03-03 02:30:36 +0200

    Enemy system

--- Personal/Balekoth/Balekoth/Program.cs
+++ Personal/Balekoth/Balekoth/Program.cs
@@ -41,6 +41,7 @@ namespace Balekoth
 
         static void MiniTitle()
         {
+            Console.Clear();
             Console.ForegroundColor = ConsoleColor.Cyan;
             MiddleWriteLine("────────────────────────────");
             MiddleWriteLine("| ╔╗ ╔═╗╦  ╔═╗╦╔═╔═╗╔╦╗╦ ╦ |");
@@ -52,9 +53,11 @@ namespace Balekoth
         }
         static void Main(string[] args)
         {
+            int mainMenuSel = 0;
             string[] randomUser = { "Skeleton", "Human", "Goblin", "Elf" };
             Random rng = new Random();
             int userRandom = rng.Next(0, randomUser.Length);
+            int hpNumber = rng.Next(50, 100);
             int strNumber = rng.Next(3, 12);
             int intNumber = rng.Next(3, 12);
             int wisNumber = rng.Next(3, 12);
@@ -68,40 +71,106 @@ namespace Balekoth
 
             //Introduction
             MiniTitle();
-            MiddleWriteLine("Welcome to the land of the Balekoth!");
+            MiddleWriteLine("Welcome to the land of Balekoth!");
             MiddleWriteLine("There are more than a hundred dungeons and caves in this land.");
             MiddleWriteLine("Be the one to conquer them all and become the master of dungeons and caves.");
             MiddleWriteLine("");
             Console.ReadKey();
-            Console.Clear();
 
             //Generated character sheet
-            CharacterSheet(randomUser, userRandom, strNumber, intNumber, wisNumber, dexNumber, conNumber, chaNumber);
+            CharacterSheet(hpNumber, randomUser, userRandom, strNumber, intNumber, wisNumber, dexNumber, conNumber, chaNumber);
             Console.ReadKey();
-            Console.Clear();
 
             //Main Menu
             MiniTitle();
 
-            MiddleWriteLine("[1] Market\n");
-            MiddleWriteLine("[2] Guild Room\n");
-            MiddleWriteLine("[3] Adventure\n");
-            MiddleWriteLine("[4] Character Sheet");
+            MiddleWriteLine("[1]        Adventure");
+            MiddleWriteLine("[2]           Market");
+            MiddleWriteLine("[3]       Guild Room");
+            MiddleWriteLine("[4]  Character Sheet");
+            ConsoleKey press = Console.ReadKey(false).Key;
+            switch (press)
+            {
+                case ConsoleKey.D1:
+                case ConsoleKey.NumPad1:
+                    mainMenuSel = 1;
+                    break;
+                case ConsoleKey.D2:
+                case ConsoleKey.NumPad2:
+                    mainMenuSel = 2;
+                    break;
+                case ConsoleKey.D3:
+                case ConsoleKey.NumPad3:
+                    mainMenuSel = 3;
+                    break;
+                case ConsoleKey.D4:
+                case ConsoleKey.NumPad4:
+                    mainMenuSel = 4;
+                    break;
+            }
+
+            if (mainMenuSel == 1)
+            {
+                MiniTitle();
+                Console.WriteLine("You have encountered a skeleton on your travels");
+
+                MiniTitle();
+                bool isKilled = false;
+                int userHealth = hpNumber;
+                int enemyHealth = rng.Next(5, 25);
+                do
+                {
+                    MiniTitle();
+                    MiddleWriteLine("!Skeleton!");
+                    Console.SetCursorPosition((Console.WindowWidth - (9 + enemyHealth.ToString().Length)) / 2, Console.CursorTop);
+                    Console.WriteLine("Enemy HP: {0}\n", enemyHealth);
+                    Console.SetCursorPosition((Console.WindowWidth - (8 + userHealth.ToString().Length)) / 2, Console.CursorTop);
+                    Console.WriteLine("Your HP: {0}", userHealth);
+                    MiddleWriteLine("Any key to attack!");
+                    Console.ReadKey();
+                    int enemySTR = rng.Next(2, 8);
+                    userHealth -= enemySTR;
+                    enemyHealth -= strNumber;
+                } while (enemyHealth > 0);
+
+                MiddleWriteLine("Enemy dead");
+                Console.ReadKey();
+            }
+
+            if (mainMenuSel == 2)
+            {
+                Console.WriteLine("");
+            }
+
+            if (mainMenuSel == 3)
+            {
+                Console.WriteLine("");
+            }
+
+            if (mainMenuSel == 4)
+            {
+                Console.WriteLine("");
+            }
+
             Console.ReadKey();
+            Console.Clear();
+
         }
 
-        static void CharacterSheet(string[] randomUser, int userRandom, int strNumber, int intNumber, int wisNumber, int dexNumber, int conNumber , int chaNumber)
+        static void CharacterSheet(int hpNumber, string[] randomUser, int userRandom, int strNumber, int intNumber, int wisNumber, int dexNumber, int conNumber , int chaNumber)
         {
 
             //Character sheet generation
             MiniTitle();
-            MiddleWriteLine("This is your generated character sheet:");
+            MiddleWriteLine("This is your character sheet:");
             Console.WriteLine();
 
             Console.SetCursorPosition((Console.WindowWidth - (5 + randomUser[userRandom].Length)) / 2, Console.CursorTop);
             Console.Write("Race: ");
             Console.Write("{0}\n", randomUser[userRandom]);
-
+            Console.SetCursorPosition((Console.WindowWidth - (3 + hpNumber.ToString().Length)) / 2, Console.CursorTop);
+            Console.Write("HP: ");
+            Console.Write("{0}\n\n", hpNumber);
 
             Console.SetCursorPosition((Console.WindowWidth - (4 + strNumber.ToString().Length)) / 2, Console.CursorTop);
             Console.Write("STR: ");
