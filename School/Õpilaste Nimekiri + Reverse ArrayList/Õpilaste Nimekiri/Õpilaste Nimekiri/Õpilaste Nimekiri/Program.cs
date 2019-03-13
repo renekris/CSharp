aad6b87683c53a33afcd95f0360f1f83373e9d47
Renekris <renekrispohlak@gmail.com>
2019-03-13 18:14:19 +0200

    Added list

--- School/Õpilaste Nimekiri + Reverse ArrayList/Õpilaste Nimekiri/Õpilaste Nimekiri/Õpilaste Nimekiri/Program.cs
+++ School/Õpilaste Nimekiri + Reverse ArrayList/Õpilaste Nimekiri/Õpilaste Nimekiri/Õpilaste Nimekiri/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,8 +9,38 @@ namespace Õpilaste_Nimekiri
 {
     class Program
     {
+        static void StudentAmount(int amount)
+        {
+            Console.ForegroundColor = ConsoleColor.DarkRed;
+            Console.WriteLine("Sinu klassis on {0} õpilast\n", amount);
+            Console.ResetColor();
+        }
         static void Main(string[] args)
         {
+            /* 1~~
+             * Õpilaste arvu ei ole antud, õpetaja ise otsustab.
+             * Loo programm, kus õpetaja saab salvestada nimekirja õpilaste nimed, hinded ja puudumised.
+             * Lisaks on tal võimalik nimekirjast otsida õpilast nime järgi ning siis näha tema hindeid ja puudumisi
+             * Võimalik kuvada kogu õpilaste nimekiri tulemustega.
+             *
+             * 2~~
+             * Küsi kasutajalt arve, kuni ta sisestab nulli.
+             * Salvesta ArrayListi.
+             * Väljasta need arvud tagurpidises järjekorras.
+             */
+            List<string> studentsList = new List<string>();
+            studentsList.Add("Juku");
+            studentsList.Add("Madis");
+            studentsList.Add("Joonas");
+            studentsList.Add("Kohuke");
+            studentsList.Add("Alfa USB Network Adapter 2.4GHz Wifi, RT3070 chip (Monitoring + packet injection)");
+            StudentAmount(studentsList.Count);
+            
+
+            int studentAmount = int.Parse(Console.ReadLine());
+            Console.WriteLine();
+
+            Console.ReadKey();
         }
     }
 }
