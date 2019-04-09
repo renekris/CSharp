94ef39f4386410f6d24fa8d44bd1a822f7483648
Renekris <renekrispohlak@gmail.com>
2019-04-09 17:29:43 +0300

    Initial commit

---
+++ School/New folder/StreamWriter koos ruututega/StreamWriter koos ruututega/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StreamWriter_koos_ruututega
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             * Tekita programmi abil fail,
+             * milles oleksid arvud ja nende ruudud
+             * ühest kahekümneni.
+             */
+            
+        }
+    }
+}
