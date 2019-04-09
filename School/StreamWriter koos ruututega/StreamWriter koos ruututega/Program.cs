a5facf056b1a5f3b3d65340d2211e5f991da300e
Renekris <renekrispohlak@gmail.com>
2019-04-09 19:15:07 +0300

    Kirjutab infot + näitab seda

--- School/StreamWriter koos ruututega/StreamWriter koos ruututega/Program.cs
+++ School/StreamWriter koos ruututega/StreamWriter koos ruututega/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,7 +16,34 @@ namespace StreamWriter_koos_ruututega
              * milles oleksid arvud ja nende ruudud
              * ühest kahekümneni.
              */
-            
+            while (true)
+            {
+                Console.WriteLine("[1]> Sisesta arv\n" +
+                                  "[2]> Ava fail kuhu kirjutas");
+                Console.Clear();
+                Console.Write("Sisesta mitmendani:");
+                int kordus = int.Parse(Console.ReadLine());
+                using (StreamWriter writer = new StreamWriter(@"data.txt"))
+                {
+                    for (int i = 0; i < kordus; i++)
+                    {
+                        writer.WriteLine("{0, 6:G25}E2 = {1, 6:G25}", i + 1, Math.Pow(i + 1, 2));
+                    }
+                    writer.Close();
+                }
+
+                using (StreamReader reader = new StreamReader(@"data.txt"))
+                {
+                    string line = reader.ReadLine();
+                    while (line != null)
+                    {
+                        Console.WriteLine(line);
+                        line = reader.ReadLine();
+                    }
+                    reader.Close();
+                }
+                Console.ReadKey();
+            }
         }
     }
 }
