e6b449c1742dc7d528d41a0ef1eeb70b620a9df6
Renekris <renekrispohlak@gmail.com>
2019-04-14 02:32:05 +0300

    Refactoring and foolproofing

--- Personal/Automatic Youtube Downloader/Automatic Youtube Downloader/Program.cs
+++ Personal/Automatic Youtube Downloader/Automatic Youtube Downloader/Program.cs
@@ -9,30 +9,51 @@ using System.Text;
 using System.Threading.Tasks;
 using System.IO;
 using System.Threading;
+using System.Windows.Forms;
+using Microsoft.Win32;
 
 namespace Automatic_Youtube_Downloader
 {
     class Program
     {
-        private static string[] filesCheck = new[] { "youtube-audio.py", "youtube-video.py" };
-        private static string urlPath = "URLS.txt";
-        private static List<string> URLS = new List<string>();
+        private static List<string> urls = new List<string>();
+        private static string[] filesCheck = new[] { "youtube-audio.py", "youtube-video.py", "/Windows/py.exe" };
+        private static string urlPath = "urls.txt";
 
         static void IsRunning(Process status)
         {
             while (!status.HasExited)
                 Thread.Sleep(250);
         }
-        static void Main(string[] args)
+
+        static void FileCheck()
         {
-            Console.Title = "Automatic Youtube V/A Downloader";
-            Console.OutputEncoding = Encoding.Unicode;
-            Console.InputEncoding = Encoding.Unicode;
             foreach (string s in filesCheck)
             {
                 if (!File.Exists(s))
+                {
                     Console.WriteLine("Puudu on vajalik fail {0}", s);
+                    if (s == "/Windows/py.exe")
+                    {
+                        Console.WriteLine("Installi endale Python 3");
+                        Process.Start("https://www.python.org/downloads/");
+                    }
+                    Console.ReadKey();
+                    Environment.Exit(0);
+                }
             }
+        }
+        static void Main(string[] args)
+        {
+            try
+            {
+
+            Console.Title = "Automatic Youtube V/A Downloader";
+            Console.OutputEncoding = Encoding.Unicode;
+            Console.InputEncoding = Encoding.Unicode;
+            //Kontrollib failide olemasolu
+
+            FileCheck();
             //Teeb faili kui see puudub
             if (!File.Exists(urlPath))
                 File.Create(urlPath).Dispose();
@@ -41,35 +62,47 @@ namespace Automatic_Youtube_Downloader
                               "Salvesta ja sule fail kui oled valmis allalaadima");
             //Ei jätka kuni failid pannakse kinni
             IsRunning(Process.Start(urlPath));
-            try
-            {
-                IsRunning(Process.Start(@"pip3", @"install pytube"));
-                IsRunning(Process.Start(@"pip3", @"install youtube-dl"));
-            }
-            catch (Exception)
-            {
-                Console.WriteLine("Installi omale Python 3 ja pip 3");
-            }
+            IsRunning(Process.Start(@"py.exe", @"-m pip install pytube"));
+            IsRunning(Process.Start(@"py.exe", @"-m pip install youtube-dl"));
 
             //Loeb sisestatud URL'id
             using (StreamReader reader = new StreamReader(urlPath))
             {
                 while (!reader.EndOfStream)
-                    URLS.Add(reader.ReadLine());
+                    urls.Add(reader.ReadLine());
+                reader.Close();
             }
 
             //Kontrollib korra veel faili olemasolu
             if (File.Exists(urlPath))
             {
-                foreach (var VARIABLE in URLS)
+                foreach (var VARIABLE in urls)
                 {
                     //Laeb videod alla
-                    IsRunning(Process.Start(@"youtube-video.py", @"-url " + VARIABLE));
+                    Console.WriteLine(urls.IndexOf(VARIABLE) + 1 + "/" + urls.Count);
+                    IsRunning(Process.Start(@"py.exe", @"youtube-video.py --url " + VARIABLE));
+                    Console.Clear();
                 }
+                Console.Clear();
                 Console.WriteLine("Kõik videod alla laetud!");
+                DialogResult ans = MessageBox.Show("Kas sa soovid vaadata allalaetud faile?", "Kausta avamine", MessageBoxButtons.YesNo, MessageBoxIcon.Question, MessageBoxDefaultButton.Button2);
+                if (ans == DialogResult.Yes)
+                {
+                    Process.Start("explorer.exe", Path.GetRandomFileName());
+                }
+                else
+                {
+                    Environment.Exit(0);
+                }
             }
-
-            Console.WriteLine("Fail on kustutatud");
+            Environment.Exit(0);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                Console.ReadKey();
+                throw;
+            }
         }
     }
 }
--- Personal/Automatic Youtube Downloader/Automatic Youtube Downloader/bin/Debug/youtube-video.py
+++ Personal/Automatic Youtube Downloader/Automatic Youtube Downloader/bin/Debug/youtube-video.py
@@ -2,7 +2,7 @@ import argparse
 from pytube import YouTube
 
 parser = argparse.ArgumentParser()
-parser.add_argument('-url', type=str)
+parser.add_argument('--url', type=str)
 args = parser.parse_args()
 
 YouTube(args.url).streams.first().download()
