b68bd04b392ffcf46be09c3fdc4b0f73bbd9c0af
Renekris <renekrispohlak@gmail.com>
2019-04-17 16:56:10 +0300

    Initial commit

---
+++ School/Vanemad/FizzBuzz/FizzBuzz/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FizzBuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i <= 100; i++)
+            {
+                Console.Write(i + ">");
+                if (i % 3 == 0)
+                    Console.Write("Fizz");
+                if (i % 5 == 0)
+                    Console.Write("Buzz");
+                Console.WriteLine("\n");
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
