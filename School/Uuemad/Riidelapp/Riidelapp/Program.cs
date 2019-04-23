4011f6959450d998c18f0a493e4ea19ee6671396
Renekris <renekrispohlak@gmail.com>
2019-04-23 17:33:00 +0300

    Initial Commit

---
+++ School/Uuemad/Riidelapp/Riidelapp/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Riidelapp
+{
+    /*
+     * kodutöö - Loo programm, kus on eraldi klass riidelapi andmete hoidmiseks: pikkus, laius, toon Klassis peab olema käsklus lapi andmete väljatrükiks,
+     * käsklus lapi pindala arvutamiseks, meetod (alamprogramm) lapi poolitamiseks: pikem külg tehakse poole lühemaks.
+     * Lisaks peab poolitamise meetod lisaks algse lapi poolitamisele väljastama ka uue samasuguse algsest poole väiksema eksemplari.
+     * Peab olema teine poolitusmeetod, kus saab määrata, mitme protsendi peale lõigatakse pikem külg.
+     * Peaprogrammis saab kasutaja sisestada andmed ning lõpuks väljastatakse tulemused. Lisaks täiendada tunnis loodud ülesannet niimoodi,
+     * et kasutaja saaks lisada ise andmeid Listi.
+     */
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
